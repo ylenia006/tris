94070188513d76da047b5c0129b7bbfe445ff197
ylenia bernardi <yleniabernardi1@gmail.com>
2023-03-04 09:44:32 +0100

    finito.

--- static/js/tris.js
+++ static/js/tris.js
@@ -1,136 +1,81 @@
-let cells = document.querySelectorAll(".button");
-let resetBtn = document.querySelector(".resetBtn");
-let isGameFinish = false;
-
-function checkWinner(player) {
-	if (grid[0][0] === player && grid[1][1] === player && grid[2][2] === player) {
-		isGameFinish = true;
-		return true;
-	} else if (
-		grid[0][2] === player &&
-		grid[1][1] === player &&
-		grid[2][0] === player
-	) {
-		isGameFinish = true;
-		return true;
-	} else if (
-		grid[0][0] === player &&
-		grid[1][0] === player &&
-		grid[2][0] === player
-	) {
-		isGameFinish = true;
-		return true;
-	} else if (
-		grid[0][1] === player &&
-		grid[1][1] === player &&
-		grid[2][1] === player
-	) {
-		isGameFinish = true;
-		return true;
-	} else if (
-		grid[0][2] === player &&
-		grid[1][2] === player &&
-		grid[2][2] === player
-	) {
-		isGameFinish = true;
-		return true;
-	} else if (
-		grid[0][0] === player &&
-		grid[0][1] === player &&
-		grid[0][2] === player
-	) {
-		isGameFinish = true;
-		return true;
-	} else if (
-		grid[1][0] === player &&
-		grid[1][1] === player &&
-		grid[1][2] === player
-	) {
-		isGameFinish = true;
-		return true;
-	} else if (
-		grid[2][0] === player &&
-		grid[2][1] === player &&
-		grid[2][2] === player
-	) {
-		isGameFinish = true;
-		return true;
-	} else {
-		//isGameFinish = true;
-		return false;
-	}
-}
-
-function RefreshWindow() {
-	window.location.reload(true);
-}
-
-resetBtn.addEventListener("click", function (e) {
-	e.preventDefault();
-	RefreshWindow();
-});
-
-
-let currentPlayer = "X";
 let grid = [
 	[null, null, null],
 	[null, null, null],
-	[null, null, null],
-];
+	[null, null, null]
+]
 
-let count = 0;
+let cells = document.querySelectorAll(".button")
+let isXwritten = false
+let xPlayer = document.getElementById("x")
+let oPlayer = document.getElementById("o")
+let resetBtn = document.getElementById("resetBtn")
+let thereIsWinner = false
 
-cellIsOccupied = true;
-let v1, v2;
+xPlayer.classList.add("img-oversize")
 
-for (let i = 0; i < cells.length; i++) {
-	cells[i].addEventListener("click", function (e) {
-		e.preventDefault();
+function checkForVictory() {
+	for (let i = 0; i < 3; i++) {
+        if (grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]) {
+            return grid[i][0]
+        }
+    }
 
-		console.log(`valore della i --> ${i}`);
-		console.log(`valore di player --> ${currentPlayer}`);
-
-		cellIsOccupied =
-			e.currentTarget.classList.contains("player2") ||
-			e.currentTarget.classList.contains("player1");
-
-		console.log(cellIsOccupied);
-		if (!cellIsOccupied && !isGameFinish) {
-			//implementare la condizione se la cella non è occupata e il gioco non è finito
-			count++;
-			v1 = cells[i].dataset.row;
-			v2 = cells[i].dataset.cell;
-
-			if (currentPlayer === "X") {
-				e.currentTarget.classList.toggle("player1");
-				cells[i].innerHTML="X"
-				grid[v1][v2] = "X";
-			} else {
-				e.currentTarget.classList.toggle("player2");
-				cells[i].innerHTML="O"
-				grid[v1][v2] = "O";
-			}
-		}
-
-		let win = checkWinner(currentPlayer);
-
-		if (win) {
-			document.querySelector(
-				".output"
-			).innerHTML = `hai vinto player ${currentPlayer}`;
-			document.querySelector(".output").classList.add("colorOutput");
-			//document.querySelectorAll(".button").classList.add("noClick");
-		} else if (count === 9 && !win) {
-			document.querySelector(".output").innerHTML = `pareggio`;
-			document.querySelector(".output").classList.add("colorOutput");
-			//document.querySelectorAll(".button").classList.add("noClick");
-		}
-
-		//punto 6
-		if (currentPlayer === "X") {
-			currentPlayer = "O";
-		} else {
-			currentPlayer = "X";
-		}
-	});
+    for (let i = 0; i < 3; i++) {
+        if (grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i]) {
+            return grid[0][i]
+        }
+    }
+    
+    if (grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]) {
+        return grid[0][0]
+    }
+    if (grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]) {
+        return grid[0][2]
+    }
 }
+
+resetBtn.addEventListener("click", () => {
+	window.location.reload(true)
+})
+
+
+cells.forEach(cell => cell.addEventListener("click", (e) => {
+	let target = e.currentTarget
+	let row = parseInt(target.getAttribute("data-row")) - 1
+	let cell = parseInt(target.getAttribute("data-cell")) - 1
+	if (!thereIsWinner) {
+		if (isXwritten) {
+			xPlayer.classList.add("img-oversize")
+			oPlayer.classList.remove("img-oversize")
+		} else {
+			xPlayer.classList.remove("img-oversize")
+			oPlayer.classList.add("img-oversize")
+		}
+
+		if (!grid[row][cell]) {
+			if (!isXwritten) {
+				isXwritten = true
+				target.style.backgroundImage = "url(static/img/caramelos-breaking-bad-removebg-preview.png)"
+				grid[row][cell] = "X"
+			} else {
+				isXwritten = false
+				target.style.backgroundImage = "url(static/img/antigas-removebg-preview.png)"
+				grid[row][cell] = "O"
+			}
+
+			if (checkForVictory() == "X") {
+				document.getElementById("winner").style.display = "flex"
+				document.getElementById("winner-image").style.backgroundImage = "url(static/img/caramelos-breaking-bad-removebg-preview.png)"
+				thereIsWinner = true
+			} else if (checkForVictory() == "O") {
+				document.getElementById("winner").style.display = "flex"
+				document.getElementById("winner-image").style.backgroundImage = "url(static/img/antigas-removebg-preview.png)"
+				thereIsWinner = true
+			}
+		} else {
+			alert("Cell Occupied")
+		}
+	}
+	
+
+}))
